b6533133543dda3e38af1a4285df8c6482eec4a4
merlinmarijn <ratched10@hotmail.nl>
2019-09-12 11:32:54 +0200

    v1

---
+++ b1l2_whatsmyname/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace b1l2_whatsmyname
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
