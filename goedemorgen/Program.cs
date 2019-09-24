8856604b10f50cb64a27d2a54c570df16b4cc392
merlinmarijn <ratched10@hotmail.nl>
2019-09-24 09:52:10 +0200

    1.0002

--- goedemorgen/Program.cs
+++ goedemorgen/Program.cs
@@ -4,9 +4,31 @@ namespace goedemorgen
 {
     class Program
     {
+        int lefthand=0;
+        int righthand;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Console.Clear();
+            Console.WriteLine("############################################");
+            Console.WriteLine("#######     Rock Paper Scissors     ########");
+            Console.WriteLine("############################################");
+            Console.ReadLine(); //input van gebruiker
+        }
+        void RunGame(){
+            //Console.WriteLine("test");
+            //if(lefthand==0){Console.WriteLine("WORKED");}
+            /*if(lefthand==1&&righthand==2){
+
+            } else if (lefthand==2&&righthand==3){
+
+            } else if (lefthand==3&&righthand==1){
+
+            } else if(lefthand==righthand){
+                Console.WriteLine("REDO");
+            } else {
+                Console.WriteLine("you lost");
+            }*/
         }
     }
 }
